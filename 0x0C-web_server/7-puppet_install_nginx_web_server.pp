df505ee4144b45cb3dad9c5253c794e7ca962ae1
raniahamoudabee <raniahamouda222@gmail.com>
2024-05-11 05:20:48 -0700

    0x0C-web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,54 @@
-# Puppet manifest to install nginx
-package { 'nginx':
-  ensure => installed,
-}
+#!/usr/bin/env bash
+# Configure your Nginx server so that /redirect_me is redirecting to another page.
+# Handle 404 errors
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
+# Update and Install nginx
+sudo apt -y update
+sudo apt -y install nginx
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
+# Remove and replace /var/www to store static content
+# Created /var/www/school/errors to store 404 error message
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+rm -rf school
+mkdir school
+mkdir school/errors
+touch 404.html
+echo "Ceci n'est pas une page" > school/errors/404.html
+
+touch school/index.html
+echo "Hello World!" > school/index.html
+
+sudo rm -rf /var/www/school
+sudo mv school /var/www/
+
+# Change the default configuration of nginx
+# Add redirection for localhost/redirect_me
+# Return  404.html in case of a 404 error (page not found)
+
+touch default
+printf %s "server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/school;
+        index index.html;
+
+        location /redirect_me {
+            return 301 https://youtube.com;
+        }
+
+        error_page 404 /404.html;
+        location = /404.html {
+            root /var/www/school/errors/;
+        }
 }
+" > default
+
+# Remove and replace existing configurations
+sudo rm -rf /etc/nginx/sites-available/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo mv default /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+
+# Restart nginx
+sudo service nginx restart
